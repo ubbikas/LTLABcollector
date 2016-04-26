a4cad8231ed94d723061a8d55554080f2d09a38f
ubbikas <ubbikas@gmail.com>
2016-04-26 09:22:39 +0300

    status update + test printing

--- LTLABview_collector.R
+++ LTLABview_collector.R
@@ -10,7 +10,7 @@ if (Sys.info()[[4]] == "LTLAB-DEV")
   setwd("C:/R/GitHub/LTLABcollector")
 
 
-refresh_rate <- 2
+refresh_rate <- 3
 last_db_update_time <- 0
 update_count <- 0
 
@@ -29,17 +29,26 @@ conn_status <- list(AX_DB_TBL = "NOT_OK",
                     SMT_PROJ = "NOT_OK",
                     PP_DATA = "NOT_OK",
                     UPDATE = "2015-01-01 01:00:00 EEST")
-
+last_status <- data.frame()
 
 # functions saves current connectios status to csv file
-status_to_csv = function() {
+status_to_csv <- function() {
   data <- data.frame(AX_DB_TBL = conn_status$AX_DB_TBL,
                      SMT_DB = conn_status$SMT_DB,
                      SMT_PROJ = conn_status$SMT_PROJ,
                      PP_DATA = conn_status$PP_DATA)
-  write.csv(data, "status.csv", row.names = FALSE)
+  last_status <- data.frame(AX_DB_TBL = last_status$AX_DB_TBL,
+                            SMT_DB = last_status$SMT_DB,
+                            SMT_PROJ = last_status$SMT_PROJ,
+                            PP_DATA = last_status$PP_DATA) 
+  if (!identical(data, last_status)) {
+    last_status <<- data
+    print("Status change:")
+    print(data)
+    write.csv(data, "status.csv", row.names = FALSE)
+  }
 }
-
+status_to_csv()
 
 # conveyorIDlist - list of the previous IDs in production, if some of them 
 # changes it means they are finished and are writen to finished roduction 
@@ -100,21 +109,26 @@ PCBfinishedtoCSV <- function(idlist) {
 
 # next section needs to be looped forever -------------------------------------
 while (TRUE) {
+Sys.sleep(refresh_rate)
+print("-----------------------------------------------------")
 
+system.time({
 # checking if tracedata DB from production exists
 if(file.exists(tracedata_DB_name)) {
   conn_status$AX_DB_TBL <- "OK"
   tracedata_DB <- src_sqlite(tracedata_DB_name)
+  print("AX_DB_TBL - OK")
 } else {
   conn_status$AX_DB_TBL <- "NOT_OK"
   status_to_csv()
-  Sys.sleep(refresh_rate)
+  print("AX_DB_TBL - NOT OK!!!!")
   next()
 }
 
 
 if (last_db_update_time == 0) {
   tryCatch({
+    print("First update")
     transportmap <- tbl(tracedata_DB, sql("SELECT * FROM transportmap"))
     current_conveyor <- as.data.frame(
                           transportmap %>% 
@@ -130,7 +144,6 @@ if (last_db_update_time == 0) {
                print("DB is busy 1")
                conn_status$AX_DB_TBL <- "NOT_OK"
                status_to_csv()
-               Sys.sleep(refresh_rate)
                next()
              }
   )
@@ -143,34 +156,38 @@ if(file.exists(ltlab_smt_db)) {
     smt_DB <- src_sqlite(ltlab_smt_db)
     projects <- tbl(smt_DB, sql("SELECT * FROM PROJEKTAI"))
     conn_status$SMT_DB <- "OK"
+    print("SMT_DB - OK")
   }, error = function(e) {
                print("SMT DB is busy")
                conn_status$SMT_DB <- "NOT_OK"
                status_to_csv()
-               Sys.sleep(refresh_rate)
                next()
              }
   )
 } else {
   conn_status$SMT_DB <- "NOT_OK"
   status_to_csv()
-  Sys.sleep(refresh_rate)
+  print("SMT_DB - NOT OK!!!!")
   next()
 }
 
-
+print(paste("Current project:", current_project))
+  
 tryCatch({
   db_update_time <- file.info(tracedata_DB_name)$mtime
-  if ((as.numeric(Sys.time()) - as.numeric(db_update_time) > 3600) && 
+  if ((as.numeric(Sys.time()) - as.numeric(db_update_time) > 15) && 
       (dim(current_conveyor)[1] == 0)) {
     current_project <- NULL
-    Sys.sleep(refresh_rate)
+    print("No production!!!!")
+    status_to_csv()
     next()
   }
   db_update_time_diff <- as.numeric(db_update_time) - as.numeric(last_db_update_time)
+  last_db_update_time <- db_update_time
   update_count <- c(update_count, db_update_time_diff) %>%
                   tail(2)
   if(update_count[1] > 0 && update_count[2] == 0){
+    print("DB update found")
     transportmap_new <- as.data.frame(
                           transportmap %>% 
                           filter(lPos %in% c(1:49)) %>%
@@ -180,6 +197,7 @@ tryCatch({
     current_conveyor_pcb_pos <- current_conveyor %>% 
                                 select(idPCB, lPos)
     if(!identical(transportmap_new, current_conveyor_pcb_pos)) {
+      print("Transport IDs changed!")
       current_conveyor <- as.data.frame(
                             transportmap %>% 
                             filter(lPos %in% c(1:49)) %>%
@@ -193,19 +211,29 @@ tryCatch({
         current_project <- as.data.frame(current_conveyor %>%
                                          select(strPP))[1,]      
       }
+      print(current_conveyor)
+    } else {
+      print("No transport change!!!!")
+      status_to_csv()
+      next()
     }
+  } else {
+    print("No DB update!!!!")
+    status_to_csv()
+    next()
   }
-  last_db_update_time <- db_update_time
 }, error = function(e) {
              print("DB is busy 2")
              conn_status$AX_DB_TBL <- "NOT_OK"
              status_to_csv()
-             Sys.sleep(refresh_rate)
              next()
            }
 )
 
-Sys.sleep(refresh_rate)
+print("Cycle finished!!!!!!!!!!!!!!!!")
+}) -> timing
+
+print(timing)
 }
 
 
